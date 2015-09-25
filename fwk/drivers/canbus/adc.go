266273bbcdb07ec1df1e88f81cf4324d9c49cd72
Sebastien Binet <binet@cern.ch>
2015-09-25 19:17:14 +0200

    fwk/drivers/canbus: remove adc-write-tx-type

--- fwk/drivers/canbus/adc.go
+++ fwk/drivers/canbus/adc.go
@@ -1,10 +1,6 @@
 package canbus
 
 import (
-	"bytes"
-	"fmt"
-	"io"
-
 	"github.com/sbinet/lsst-ccs/fwk"
 )
 
@@ -38,68 +34,8 @@ func (*ADC) Volts(adc int) float64 {
 }
 
 func (adc *ADC) init() error {
-	if true {
-		return nil
-	}
-
-	const sz = 1 // len of adc.tx
-	const subchannel = 2
-	for _, channel := range []int{0x1801, 0x1802} {
-		n, err := adc.bus.conn.Write(
-			Command{
-				Name: Wsdo,
-				Data: []byte(fmt.Sprintf(
-					"%x,%x,%x,%x,%x",
-					adc.node, channel, subchannel, sz, adc.tx,
-				)),
-			}.bytes(),
-		)
-		if err != nil {
-			return err
-		}
-		if n <= 0 {
-			return io.ErrShortWrite
-		}
-
-		buf := make([]byte, 1024)
-		n, err = adc.bus.conn.Read(buf)
-		if err != nil {
-			return err
-		}
-		if n <= 0 {
-			return io.ErrShortBuffer
-		}
-		buf = buf[:n]
-		cmd := newCommand(buf)
-		adc.Infof("channel: 0x%x => %v\n", channel, cmd)
-		switch cmd.Name {
-		case Wsdo:
-			node := 0
-			ecode := 0
-			n, err = fmt.Fscanf(
-				bytes.NewReader(cmd.Data),
-				"%x,%x",
-				&ecode,
-				&node,
-			)
-			if err != nil {
-				return err
-			}
-			if n <= 0 {
-				return io.ErrShortBuffer
-			}
-			if node != adc.node {
-				return fmt.Errorf("unexpected node. got=0x%x want=0x%x", node, adc.node)
-			}
-			if ecode != 0 {
-				return fmt.Errorf("canbus error (ecode=%d)", ecode)
-			}
-		default:
-			return fmt.Errorf("unexpected command %v", cmd)
-		}
-	}
-
-	return nil
+	var err error
+	return err
 }
 
 func NewADC(name, serial string, tx int) *ADC {
