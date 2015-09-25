4f1dca934c43a5b0b2ce1c5838d8f697cc40599d
Sebastien Binet <binet@cern.ch>
2015-09-25 19:15:21 +0200

    fwk/drivers/canbus: devices -> adc+dac

--- fwk/drivers/canbus/adc.go
+++ fwk/drivers/canbus/adc.go
@@ -109,30 +109,3 @@ func NewADC(name, serial string, tx int) *ADC {
 		tx:     tx,
 	}
 }
-
-type DAC struct {
-	*fwk.Base
-	node   int
-	serial string
-	bus    *Bus
-}
-
-func (dac *DAC) Node() int {
-	return dac.node
-}
-
-func (dac *DAC) Serial() string {
-	return dac.serial
-}
-
-func (dac *DAC) init() error {
-	var err error
-	return err
-}
-
-func NewDAC(name, serial string) *DAC {
-	return &DAC{
-		Base:   fwk.NewBase(name),
-		serial: serial,
-	}
-}
