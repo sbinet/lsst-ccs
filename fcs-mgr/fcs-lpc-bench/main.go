718bc227f13c7eae2298060bb4fb50e3cbe13106
Sebastien Binet <binet@cern.ch>
2015-10-05 09:44:00 +0200

    fcs-lpc-bench: add switches for mock+port

--- fcs-mgr/fcs-lpc-bench/main.go
+++ fcs-mgr/fcs-lpc-bench/main.go
@@ -1,21 +1,31 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/sbinet/lsst-ccs/fwk"
 	"github.com/sbinet/lsst-ccs/fwk/drivers/canbus"
 	"github.com/sbinet/lsst-ccs/fwk/drivers/hd2001"
 )
 
-const (
-	port = 50000
+var (
+	mock = flag.Bool("mock", false, "switch to use mock-canbus")
+	port = flag.Int("port", 50000, "port for c-wrapper")
 )
 
 func main() {
 
+	flag.Parse()
+
+	newBus := canbus.New
+	if *mock {
+		newBus = canbus.NewMock
+	}
+
 	app, err := fwk.New(
 		"lpc",
-		canbus.NewMock(
-			"canbus", port,
+		newBus(
+			"canbus", *port,
 			canbus.NewADC("ai814", "c7c80499", 0x1),
 			canbus.NewDAC("ao412", "c7c60327"),
 		),
